243c2665419adb5eff4a3e89556bda87aa407263
RaymonYip-NUC11 <2205929492@qq.com>
2024-01-24 16:21:43 +0800

    style: clang-format

--- elevation_mapping/src/user_files/getRobotPose.cpp
+++ elevation_mapping/src/user_files/getRobotPose.cpp
@@ -1,8 +1,7 @@
-#include <ros/ros.h>
-#include <tf/transform_listener.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <ros/ros.h>
 #include <tf/transform_datatypes.h>
-
+#include <tf/transform_listener.h>
 
 // 发布消息
 ros::Publisher pub;
@@ -10,57 +9,56 @@ ros::Publisher pub;
 void tfCallback(const tf::TransformListener& listener);
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "tf_listener");
-    ros::NodeHandle nh;
+  ros::init(argc, argv, "tf_listener");
+  ros::NodeHandle nh;
 
-    pub = nh.advertise<geometry_msgs::PoseStamped>("/robotPose", 10);
+  pub = nh.advertise<geometry_msgs::PoseStamped>("/robotPose", 10);
 
-    tf::TransformListener listener(ros::Duration(0.01));  // 设置缓存时间为0.01秒
+  tf::TransformListener listener(ros::Duration(0.01));  // 设置缓存时间为0.01秒
 
-    ros::Rate rate(50);  // 设置循环频率为50Hz
+  ros::Rate rate(50);  // 设置循环频率为50Hz
 
-    while (ros::ok()) {
-        tfCallback(listener);
+  while (ros::ok()) {
+    tfCallback(listener);
 
-        rate.sleep();
-    }
+    rate.sleep();
+  }
 
-    return 0;
+  return 0;
 }
 void tfCallback(const tf::TransformListener& listener) {
-    // 设置源坐标系和目标坐标系
-    std::string source_frame = "world";
-    std::string target_frame = "base_link";
+  // 设置源坐标系和目标坐标系
+  std::string source_frame = "world";
+  std::string target_frame = "base_link";
 
-    tf::StampedTransform transform;
-    try {
-        listener.waitForTransform(source_frame, target_frame, ros::Time(0), ros::Duration(0.01));
-        listener.lookupTransform(source_frame, target_frame, ros::Time(0), transform);
-    } catch (tf::TransformException& ex) {
-        ROS_ERROR("%s", ex.what());
-        return;
-    }
+  tf::StampedTransform transform;
+  try {
+    listener.waitForTransform(source_frame, target_frame, ros::Time(0), ros::Duration(0.01));
+    listener.lookupTransform(source_frame, target_frame, ros::Time(0), transform);
+  } catch (tf::TransformException& ex) {
+    ROS_ERROR("%s", ex.what());
+    return;
+  }
 
-    // 提取位置信息
-    double x = transform.getOrigin().x();
-    double y = transform.getOrigin().y();
-    double z = transform.getOrigin().z();
+  // 提取位置信息
+  double x = transform.getOrigin().x();
+  double y = transform.getOrigin().y();
+  double z = transform.getOrigin().z();
 
-    // 提取姿态信息
-    tf::Quaternion q = transform.getRotation();
-    double roll, pitch, yaw;
-    tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
+  // 提取姿态信息
+  tf::Quaternion q = transform.getRotation();
+  double roll, pitch, yaw;
+  tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
 
-    // 创建并填充geometry_msgs::PoseStamped消息
-    geometry_msgs::PoseStamped pose_msg;
-    pose_msg.header.frame_id = source_frame;
-    pose_msg.header.stamp = ros::Time::now();
-    pose_msg.pose.position.x = x;
-    pose_msg.pose.position.y = y;
-    pose_msg.pose.position.z = z;
-    pose_msg.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(roll, pitch, yaw);
+  // 创建并填充geometry_msgs::PoseStamped消息
+  geometry_msgs::PoseStamped pose_msg;
+  pose_msg.header.frame_id = source_frame;
+  pose_msg.header.stamp = ros::Time::now();
+  pose_msg.pose.position.x = x;
+  pose_msg.pose.position.y = y;
+  pose_msg.pose.position.z = z;
+  pose_msg.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(roll, pitch, yaw);
 
-    pub.publish(pose_msg);
-    //std::cout<< "has send pose info"<<"x: "<<x<<"y: "<<y<<"z: "<<z<< std::endl;
+  pub.publish(pose_msg);
+  // std::cout<< "has send pose info"<<"x: "<<x<<"y: "<<y<<"z: "<<z<< std::endl;
 }
-
--- elevation_mapping/src/user_files/gridMapPostProcess.cpp
+++ elevation_mapping/src/user_files/gridMapPostProcess.cpp
@@ -1,101 +1,91 @@
-#include "ros/ros.h"
-#include <iostream>
-#include <grid_map_ros/grid_map_ros.hpp>
 #include <grid_map_msgs/GridMap.h>
-#include <vector>
-#include <string>
-#include <tf/transform_listener.h>
-#include <tf/message_filter.h>
 #include <message_filters/subscriber.h>
+#include <tf/message_filter.h>
+#include <tf/transform_listener.h>
+#include <grid_map_ros/grid_map_ros.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "ros/ros.h"
 
 using namespace std;
 
-
-
-float obstatleMin, obstatleMax, mapResolution; //用来滤除的障碍物的下界和上界，在这个高度范围的会被认为是障碍物，其他地方会被认为是空地
+float obstatleMin, obstatleMax, mapResolution;  //用来滤除的障碍物的下界和上界，在这个高度范围的会被认为是障碍物，其他地方会被认为是空地
 
 grid_map::GridMap map_({"elevation"});
 ros::Publisher gridMapPub;
-void process(grid_map::GridMap &map_p);
+void process(grid_map::GridMap& map_p);
 void publish();
 
-//gridMapCallback回调函数
-void gridMapCallback(const grid_map_msgs::GridMap& msgs)
-{
-    // cout<<msgs.info.length_x<<' '<<msgs.info.length_y<<' '<<msgs.info.resolution<<endl;
-    // cout<<msgs.info.pose.position.x<<' '<<msgs.info.pose.position.y<<endl;
-    
-    grid_map::GridMapRosConverter::fromMessage(msgs, map_,{"elevation"},false,false);
-    //map_.setStartIndex({0,0});
-    //map_.setPosition({0,0});
-    process(map_);
-    publish();
+// gridMapCallback回调函数
+void gridMapCallback(const grid_map_msgs::GridMap& msgs) {
+  // cout<<msgs.info.length_x<<' '<<msgs.info.length_y<<' '<<msgs.info.resolution<<endl;
+  // cout<<msgs.info.pose.position.x<<' '<<msgs.info.pose.position.y<<endl;
 
+  grid_map::GridMapRosConverter::fromMessage(msgs, map_, {"elevation"}, false, false);
+  // map_.setStartIndex({0,0});
+  // map_.setPosition({0,0});
+  process(map_);
+  publish();
 }
 
-void publish()
-{
+void publish() {
   map_.setTimestamp(ros::Time::now().toNSec());
   grid_map_msgs::GridMap message;
   grid_map::GridMapRosConverter::toMessage(map_, message);
   gridMapPub.publish(message);
-  //ROS_DEBUG("Grid map (timestamp %f) published.", message.info.header.stamp.toSec());
+  // ROS_DEBUG("Grid map (timestamp %f) published.", message.info.header.stamp.toSec());
 }
 
-void process(grid_map::GridMap &map_p)
-{
-    //grid map迭代器
-    grid_map::GridMapIterator iterator(map_p);
-    //遍历grid map
-    for (iterator; !iterator.isPastEnd(); ++iterator)
-    {
-        //获取当前cell的索引
-        grid_map::Index index = *iterator;
-        //获取当前cell的值
-        float value = map_p.at("elevation", index);
-        //判断当前cell是否为nan
-        if (isnan(value))
-        {
-            //如果是nan，将当前cell的值设为0
-            map_p.at("elevation", index) = 0;
-        }else if(value>obstatleMin){
-            map_p.at("elevation", index) = 2.6;
-            std::cout<<"obstacle_min: "<<obstatleMin<<std::endl;
-        }else{
-            map_p.at("elevation", index) = 0;
-            
-        }
+void process(grid_map::GridMap& map_p) {
+  // grid map迭代器
+  grid_map::GridMapIterator iterator(map_p);
+  //遍历grid map
+  for (iterator; !iterator.isPastEnd(); ++iterator) {
+    //获取当前cell的索引
+    grid_map::Index index = *iterator;
+    //获取当前cell的值
+    float value = map_p.at("elevation", index);
+    //判断当前cell是否为nan
+    if (isnan(value)) {
+      //如果是nan，将当前cell的值设为0
+      map_p.at("elevation", index) = 0;
+    } else if (value > obstatleMin) {
+      map_p.at("elevation", index) = 2.6;
+      std::cout << "obstacle_min: " << obstatleMin << std::endl;
+    } else {
+      map_p.at("elevation", index) = 0;
     }
+  }
 }
 
-//ros主函数
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "gridMapPostProcess");
-    ros::NodeHandle nh("~");
+// ros主函数
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "gridMapPostProcess");
+  ros::NodeHandle nh("~");
 
-    ros::Rate loop_rate(10);
+  ros::Rate loop_rate(10);
 
-      /*  Fsm param  */
-    float mapSizeX = nh.param<float>("mapSizeX", 30.0);
-    float mapSizeY = nh.param<float>("mapSizeY", 30.0);
-    mapResolution = nh.param<float>("mapResolution", 0.2);
-    obstatleMin = nh.param<float>("obstatleMin", 0.05);
-    obstatleMax = nh.param<float>("obstatleMax", 0.30);
-    std::string mapInTopicName = nh.param<std::string>("mapInTopicName", "/elevation_mapping/elevation_map");
-    std::string mapOutTopicName = nh.param<std::string>("mapOutTopicName", "/elevation_mapping/elevation_map_post_process");
-    std::string mapFrameId = nh.param<std::string>("mapFrameId", "map");
-    std::string robotFrameId = nh.param<std::string>("robotFrameId", "base_link");
+  /*  Fsm param  */
+  float mapSizeX = nh.param<float>("mapSizeX", 30.0);
+  float mapSizeY = nh.param<float>("mapSizeY", 30.0);
+  mapResolution = nh.param<float>("mapResolution", 0.2);
+  obstatleMin = nh.param<float>("obstatleMin", 0.05);
+  obstatleMax = nh.param<float>("obstatleMax", 0.30);
+  std::string mapInTopicName = nh.param<std::string>("mapInTopicName", "/elevation_mapping/elevation_map");
+  std::string mapOutTopicName = nh.param<std::string>("mapOutTopicName", "/elevation_mapping/elevation_map_post_process");
+  std::string mapFrameId = nh.param<std::string>("mapFrameId", "map");
+  std::string robotFrameId = nh.param<std::string>("robotFrameId", "base_link");
 
-    //定义gridmap
-    map_.setFrameId(robotFrameId);
-    map_.setGeometry(grid_map::Length(mapSizeX, mapSizeY), mapResolution);
-    map_.setPosition(grid_map::Position(0, 0));
-    map_.setTimestamp(ros::Time::now().toNSec());
-    ros::Subscriber gridMapSub = nh.subscribe(mapInTopicName, 1, &gridMapCallback);
-    gridMapPub = nh.advertise<grid_map_msgs::GridMap>(mapOutTopicName, 1);
+  //定义gridmap
+  map_.setFrameId(robotFrameId);
+  map_.setGeometry(grid_map::Length(mapSizeX, mapSizeY), mapResolution);
+  map_.setPosition(grid_map::Position(0, 0));
+  map_.setTimestamp(ros::Time::now().toNSec());
+  ros::Subscriber gridMapSub = nh.subscribe(mapInTopicName, 1, &gridMapCallback);
+  gridMapPub = nh.advertise<grid_map_msgs::GridMap>(mapOutTopicName, 1);
 
-    ros::spin();
-    loop_rate.sleep();
-    return 0;
+  ros::spin();
+  loop_rate.sleep();
+  return 0;
 }
